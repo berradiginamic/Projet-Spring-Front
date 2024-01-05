d852c354c720190159d99c73af3e81ddf3b83abb
Alfchristo <juppchristophe@hotmail.com>
2024-01-05 18:51:10 +0100

    - Dernière version

--- moviestar/src/pages/home/Home.js
+++ moviestar/src/pages/home/Home.js
@@ -1,11 +1,42 @@
-// src/pages/Home.js
+// Home.js
 import React from 'react';
+// eslint-disable-next-line
+import { Link } from 'react-router-dom';
+import { useSpring, animated } from 'react-spring';
+import '../../styles/home.css'; // Assurez-vous d'importer le fichier de styles CSS
 
 const Home = () => {
+    const titleAnimation = useSpring({
+        opacity: 1,
+        from: { opacity: 0 },
+        delay: 500, // Délai pour retarder l'animation
+    });
+
+    const windowAnimation = useSpring({
+        opacity: 1,
+        from: { opacity: 0 },
+        delay: 1000, // Délai pour retarder l'animation de la fenêtre
+        marginTop: 0,
+        // eslint-disable-next-line
+        from: { marginTop: 100 },
+    });
+
     return (
-        <div>
-            <h1>Welcome to the Movie Information Website</h1>
-            {/* Add any additional content for the home page */}
+        <div className="home-page">
+            <animated.h1 style={titleAnimation} className="title">
+                Bienvenue sur Film Harmonie
+            </animated.h1>
+            <img
+                src={`${process.env.PUBLIC_URL}/images/cinemaa.jpg`}
+                alt="Cinema"
+                className="full-width-image"
+            />
+
+            <animated.div style={windowAnimation} className="info-window">
+                <p>
+                    Bienvenue sur Film Harmonie - Votre Source Infinie de Cinéma 🎬✨. Explorez un univers cinématographique sans limites. Retrouvez instantanément vos réalisateurs, acteurs, et films préférés notés par le grand public. Plongez dans une variété de genres, explorez des collaborations uniques d'acteurs, et découvrez le classement des meilleurs films de tous les temps. Bienvenue sur Film Harmonie 🌟🍿.
+                </p>
+            </animated.div>
         </div>
     );
 };
